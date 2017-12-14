231ee779ed55db3642d119894863306defce69b9
jintianzhang <jintz_1215@163.com>
2017-12-14 16:02:05 +0800

    test

--- server/src/main/java/com/sf/marathon/Main.java
+++ server/src/main/java/com/sf/marathon/Main.java
@@ -1,17 +1,16 @@
 package com.sf.marathon;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@EnableAutoConfiguration
+@RestController
+@SpringBootApplication
 public class Main {
 
-    @RequestMapping("/")
-    @ResponseBody
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
     String home() {
         return "Hello World!";
     }
