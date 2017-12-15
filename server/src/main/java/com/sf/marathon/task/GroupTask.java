638335d8b841872b28222f20647982f2121ad6f6
zhongzhijun <zhongzhijun@bogon>
2017-12-15 14:22:36 +0800

    aa

--- server/src/main/java/com/sf/marathon/task/GroupTask.java
+++ server/src/main/java/com/sf/marathon/task/GroupTask.java
@@ -17,50 +17,10 @@ public class GroupTask {
     private ProMarketBaseService proMarketBaseService;
     @Autowired
     private GroupTaskBiz groupTaskBiz;
-  
+
     @Scheduled(fixedRate = 6000)
     public void executeGroupTask() {
         List<ProMarketBase> list = proMarketBaseService.findAll();
         list.forEach(pm -> groupTaskBiz.handleProMarketBase(pm));
-    }
-
-    private void handleProMarketBase(ProMarketBase pm) {
-        PackGroup pg = packGroupDao.findUnfinishGroup(pm.getId());
-        if (pg == null) {
-            createPackGroup(pm);
-        } else {
-            checkGroupIsFinish(pm, pg);
-        }
-    }
-
-    private void createPackGroup(ProMarketBase pm) {
-        PackGroup pg = new PackGroup();
-        pg.setId(UUID.randomUUID().toString());
-        pg.setPid(pm.getId());
-        pg.setBeginTime(new Date());
-
-        Calendar c = Calendar.getInstance();
-        c.add(Calendar.MINUTE, pm.getGroupDuration());
-        pg.setEndTime(c.getTime());
-        pg.setGroupNum(0);
-        pg.setFinish((byte) 0);
-        pg.setCreateTime(new Date());
-        pg.setVersion(0);
-
-        packGroupDao.save(pg);
-    }
-
-    private void checkGroupIsFinish(ProMarketBase pm, PackGroup pg) {
-        long endTime = (pg.getEndTime() == null) ? 0 : pg.getEndTime().getTime();
-        int groupNum = (pg.getGroupNum() == null) ? 0 : pg.getGroupNum();
-        int groupLimit = (pm.getGroupLimit() == null) ? 0 : pm.getGroupLimit();
-
-        if (System.currentTimeMillis() > endTime || groupNum >= groupLimit) {
-            pg.setFinish((byte) 1);
-            pg.setFinishTime(new Date());
-            packGroupDao.save(pg);
-
-            createPackGroup(pm);
-        }
     }
 }
