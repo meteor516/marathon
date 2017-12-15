3c110307c39d0c679c467b9d92b3c669fd2a1151
jintianzhang <jintz_1215@163.com>
2017-12-15 14:54:35 +0800

    update

--- server/src/main/java/com/sf/marathon/service/impl/ProMarketBaseService.java
+++ server/src/main/java/com/sf/marathon/service/impl/ProMarketBaseService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class PackService {
+public class ProMarketBaseService {
 	@Autowired
 	private ProMarketBaseRepository proMarketBaseRepository;
 
