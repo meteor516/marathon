3639719d7eab48a09f2ee637c60f6fcda5a7bd9c
jintianzhang <jintz_1215@163.com>
2017-12-15 14:22:19 +0800

    update

--- server/src/main/java/com/sf/marathon/control/ProMarketBaseController.java
+++ server/src/main/java/com/sf/marathon/control/ProMarketBaseController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sf.marathon.domain.ProMarketBase;
-import com.sf.marathon.service.ProMarketBaseService;
+import com.sf.marathon.service.impl.ProMarketBaseService;
 
 @RestController
 public class ProMarketBaseController {
