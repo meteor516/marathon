fffb26d7f00c233471eadbc92615393405a40e53
204062 <liuzhiling@sf-express.com>
2017-12-15 16:21:04 +0800

    【marathon】add result

--- server/src/main/java/com/sf/marathon/dto/Result.java
+++ server/src/main/java/com/sf/marathon/dto/Result.java
@@ -2,11 +2,34 @@ package com.sf.marathon.dto;
 
 import java.io.Serializable;
 
-public class Result implements Serializable {
+public class Result<T> implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    private String code;
-    private String cause;
+    private String errorMsg;
+    private T reponse;
+    private Boolean isSuccess;
 
+    public String getErrorMsg() {
+        return errorMsg;
+    }
 
+    public void setErrorMsg(String errorMsg) {
+        this.errorMsg = errorMsg;
+    }
+
+    public T getReponse() {
+        return reponse;
+    }
+
+    public void setReponse(T reponse) {
+        this.reponse = reponse;
+    }
+
+    public Boolean getSuccess() {
+        return isSuccess;
+    }
+
+    public void setSuccess(Boolean success) {
+        isSuccess = success;
+    }
 }
