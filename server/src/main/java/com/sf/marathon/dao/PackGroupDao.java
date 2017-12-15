7c75cfecce8dd438d9458bee2e074449d5ef0e8a
204062 <liuzhiling@sf-express.com>
2017-12-15 15:49:07 +0800

    Merge remote-tracking branch 'upstream/master'
    # Conflicts:
    #	server/src/main/java/com/sf/marathon/dao/PackGroupDao.java
    #	server/src/main/java/com/sf/marathon/service/impl/GroupTaskBizImpl.java
    #	server/src/main/java/com/sf/marathon/task/GroupTask.java

--- server/src/main/java/com/sf/marathon/dao/PackGroupDao.java
+++ server/src/main/java/com/sf/marathon/dao/PackGroupDao.java
@@ -12,7 +12,7 @@ public interface PackGroupDao extends JpaRepository<PackGroup, String> {
     @Query(value = "select u from com.sf.marathon.domain.PackGroup u where u.id=:id and u.finish = 0 ")
     PackGroup findUnfinishPackageGroupById(@Param("id") String id);
 
-    @Query(value = "select u from com.sf.marathon.domain.PackGroup u where u.pid=:pid and u.finish = 0")
+    @Query(value = "select u from com.sf.marathon.domain.PackGroup u where u.proMarketBase.id=:pid and u.finish = 0")
     List<PackGroup> findUnfinishGroup(@Param("pid") String pid);
 
 }
